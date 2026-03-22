8ad24189ec5ad4cca5a6d70758d6a9252136967c
JuanGomez507 <gomezdavid507@gmail.com>
2026-03-21 22:21:42 -0500

    se realizo la documentacion y la logica

---
+++ server_sockets/src/main/java/org/kami/ballSettings/vista/VentanaBolas.java
@@ -0,0 +1,39 @@
+package org.kami.ballSettings.vista;
+
+import javax.swing.JFrame;
+
+/**
+ * Ventana principal de la aplicación que contiene el panel donde se
+ * dibuja la animación de la bola.
+ *
+ * Esta clase extiende JFrame y se encarga de crear la ventana
+ * gráfica donde se mostrará el PanelBolas.
+ *
+ * Configura propiedades básicas de la ventana como el título,
+ * el tamaño automático, la posición en pantalla y el comportamiento
+ * al cerrarse.
+ *
+ * @author Juan
+ * @author Emma
+ * @author Cami
+ * @author Manu
+ */
+public class VentanaBolas extends JFrame {
+
+    /**
+     * Constructor de la ventana principal.
+     * Inicializa la interfaz gráfica y agrega el panel donde
+     * se dibuja la bola.
+     */
+    public VentanaBolas() {
+        setTitle("Bola TCP — Pantalla Local");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        add(new PanelBolas());
+
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+}
